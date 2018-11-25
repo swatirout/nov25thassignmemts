47dffca7c4fbb922e65f42417eca9eab854c5456
Swati Rout <>
2018-11-25 21:33:24 +0530

    wjhdfjef# with '#' will be ignored, and an empty message aborts the commit.

--- nov25/ViewController.swift
+++ nov25/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("this is for first change ")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
